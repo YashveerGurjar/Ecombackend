d67dc4b2d2ea68c5c9cd1eb38c034a4f575f4e25
yashveer <yash.gurjar@deeporion.com>
2023-11-07 13:03:15 +0530

    backend

--- Routes/User.js
+++ Routes/User.js
@@ -1,12 +1,47 @@
 const express=require('express');
 const route=express.Router();
+const {verifyToken,verifyTokenUser,verifyTokenAdmin}=require('./VerifyToken')
+const User=require('../Models/User');
+const CryptoJS= require('crypto-js');
 
-route.get('/Home',(req,resp)=>{
-    resp.send("Home is here")
-});
+route.put("/:id",verifyTokenUser,async (req,resp)=>{
+    if(req.body.password){
+        req.body.password= CryptoJS.AES.encrypt(req.body.password,process.env.pass).toString();
+    }
+    try{
+        const updateUser= await User.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        },
+        {
+            new:true
+        }
+        );
+        resp.status(200).json(updateUser);
+    }
+    catch(err){
+        resp.status(500).json(err);
+    }
+})
+route.get('/Find',verifyTokenAdmin,async(req,res)=>{
+    
+    try{
+        const data= await User.find();
+        res.status(200).json(data);    
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+route.delete('/:id',verifyTokenAdmin,async(req,res)=>{
+    
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");    
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
 
-route.post('/Product',(req,resp)=>{
-    const productData=req.body;
-    resp.send(productData.name);
-});
+
 module.exports=route;
--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-const express=require ('express');
-const app=express();
-const mongoose =require('mongoose')
-const dotenv=require('dotenv');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
 dotenv.config();
-const RouteFile=require('./Routes/User')
+const nodemailer = require("nodemailer");
+const cors=require('cors');
+const userFile = require('./Routes/User');
+const authFile = require('./Routes/Auth');
+const productFile = require('./Routes/Product');
+const stripeFile = require('./Routes/Stripe');
+
+const mymail="yash.gurjar@deeporion.com"
+const pass="kodi rlsi mpuh vzgt" 
+
+app.use(cors());
 app.use(express.json());
-app.use('/api/data',RouteFile);
+app.use('/api/User', userFile);
+app.use('/api/Auth', authFile);
+app.use('/api/product', productFile);
+app.use('/api/Stripe', stripeFile);
+
+app.post("/api/Mail",async(req,res)=>{
+    const username="admin";
+    const usermail=req.body.inputdata
+    console.log(usermail);
+    const transporter = nodemailer.createTransport({
+        service:"gmail",
+        port: 587,
+        
+        auth: {
+          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+          user: mymail,
+          pass: pass,
+        },
+      });
+      const info = await transporter.sendMail({
+        from: '"Fashion Store" <yash.gurjar@deeporion.com>', // sender address
+        to: usermail, // list of receivers
+        subject: "Latest Update", // Subject line
+       
+        html: `<b> Hello ${username} thanks for the visit </b>`, // html body
+      });
+            
+      console.log("Message sent: %s", info.messageId);
+      res.status(200).json(info);
+
+
+})
+
 
 mongoose.connect(process.env.ConUrl)
-.then(()=>console.log("connection Successful"))
-.catch((err)=>console.log(err));
+    .then(() => console.log("connection Successful"))
+    .catch((err) => console.log(err));
 
-app.listen(3000,()=>{
-console.log("server is working")
+    app.get("/", async(req, resp) => {
+        resp.send("app is working fine");
+    })
+
+app.listen(5000, () => {
+    console.log("server is working")
 });
 
 
